104a64ebc1dab377e18b436aa41179f403593537
Artmines <arturowells@gmail.com>
2026-04-27 20:07:08 -0230

    feat(init):initial release

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version("cerulean")
+games({ "gta5" })
+lua54("yes")
+client_script("@pulsar-core/exports/cl_error.lua")
+client_script("@pulsar-pwnzor/client/check.lua")
+
+client_scripts({
+	"client/**/*.lua",
+})
+
+server_scripts({
+	'@oxmysql/lib/MySQL.lua',
+	"server/**/*.lua",
+})
+
+shared_scripts({
+	"@ox_lib/init.lua",
+	"shared/config.lua",
+})
+
+ui_page("ui/dist/index.html")
+
+files({ "ui/dist/index.html", "ui/dist/*.png", "ui/dist/*.js" })
---
+++ server/component.lua
@@ -0,0 +1,53 @@
+ONLINE_CHARACTERS = {}
+
+AddEventHandler('onResourceStart', function(resource)
+	if resource == GetCurrentResourceName() then
+		Wait(1000)
+		RegisterCallbacks()
+		RegisterLocationCallbacks()
+		RegisterMiddleware()
+		Startup()
+		RegisterCommands()
+	end
+end)
+
+exports("GetLastLocation", function(source)
+	return _tempLastLocation[source] or false
+end)
+
+TablesToDecode = {
+	"Origin",
+	"Apps",
+	"Wardrobe",
+	"Jobs",
+	"Addiction",
+	"PhoneSettings",
+	"Crypto",
+	"Licenses",
+	"Alias",
+	"PhonePermissions",
+	"LaptopApps",
+	"LaptopSettings",
+	"LaptopPermissions",
+	"Animations",
+	"States",
+	"MDTHistory",
+	"MDTSuspension",
+	"Qualifications",
+	"LastClockOn",
+	"Salary",
+	"TimeClockedOn",
+	"Reputations",
+	"GangChain",
+	"Jailed",
+	"ICU",
+	"Status",
+	"Parole",
+	"LSUNDGBan",
+	"PhonePosition",
+	"Callsign",
+	"HUDConfig",
+	"DrugStates",
+	"CasinoChips",
+	-- Maybe more to be added
+}
